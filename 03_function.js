b7dcc89a20299b0eef916fa437222c883fef3953
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-02-20 09:01:08 +0530

    Create 03_function.js

---
+++ 03_function.js
@@ -0,0 +1,31 @@
+function swap(a,b)
+{
+    var temp;
+    temp=b;
+    b=a;
+    a=temp;
+    console.log(a+"\n"+b);
+
+}
+swap(10,20);
+
+
+function dateTime()
+{
+    
+    return "Current time & Date = " +new Date(); 
+
+}
+
+console.log(dateTime());
+
+
+function addition(num1,num2,num3)
+{
+    return num1+num2+num3;
+}
+
+console.log("Addition = "+addition(10,202,303));
+
+console.log(name1)
+var name1="BS";
