2ccdfcb14bc7cddb745bd0fe4fea6fb46c01c59f
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-02-23 07:18:25 +0530

    22-2-2023

---
+++ 04_string_1.js
@@ -0,0 +1,4 @@
+console.log("This is 'Suraj'");
+var fname="Suraj";
+var lname="Bhosale";
+console.log(`my first name ${fname} and last name ${lname}`);
---
+++ 05_operator.js
@@ -0,0 +1,24 @@
+var num1=15;
+var num2=10;
+console.log(`--------------------------------------------------------`);
+console.log(`Addition= ${num1+num2}`);
+console.log(`--------------------------------------------------------`);
+console.log(`subtraction = ${num1-num2}`);
+console.log(`--------------------------------------------------------`);
+console.log(`multiplication = ${num1*num2}`);
+console.log(`--------------------------------------------------------`);
+console.log(`Division = ${num1/num2}`);
+console.log(`--------------------------------------------------------`);
+console.log(`Reminder = ${num1 % num2}`);
+console.log(`--------------------------------------------------------`);
+console.log(`Pre Increment = ${++num1}`);
+console.log(`--------------------------------------------------------`);
+console.log(`Post Increment = ${num1++}`);
+console.log(`--------------------------------------------------------`);
+var int=10;
+int +=20;
+console.log(`Compound Addition = ${int}`);
+console.log(`--------------------------------------------------------`);
+console.log(`Compare the value ${int === "30"}`);
+
+
