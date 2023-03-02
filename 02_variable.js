83aacd84509687ea0803be32f88bdb8344dfcf0a
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-03-02 07:06:58 +0530

    Create 02_variable.js

---
+++ 02_variable.js
@@ -0,0 +1,18 @@
+var name="Suraj";
+var name
+console.log(name);
+
+let sname="Bhosale";
+// let sname=10;
+console.log(sname);
+
+carName="BMW";
+var carName;
+console.log(carName);
+
+// fname="Sujal";
+// let fname;
+// console.log(fname);
+
+lname="Bhosale";
+const lname=0;
