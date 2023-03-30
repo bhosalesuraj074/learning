1c1fcccc12413251574fecd3efa4b669b2b27813
bhosalesuraj074 <bhosalesuraj074@gmail.com>
2023-03-30 17:03:06 +0530

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,14 @@
+//'use strict'
+function demo(name,nS){
+let x=name;
+//console.log(x);
+return x;
+}
+console.log(demo("Suraj","bS"));
+let demo1="sda";
+delete demo1;
+
+
+const arr = ["apple","samsung"];
+let [e1, e2="vivo", e3="oneplus"] = arr;
+console.log(e1,e2,e3,);
